5aa0fb14cf382dea6fd4acc1381c0825f928f244
Yiting Wang <yw3c@>
2020-03-13 09:42:56 -0500

    add login function for client side

--- client/client.c
+++ client/client.c
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <termios.h>
+#include <unistd.h>
 #include "client.h" //the client header file
 
 int main() {
@@ -23,7 +25,7 @@ int main() {
   serverAddr.sin_port = htons(PORT);
 
   //Convert IPv4 and IPv6 address from text to binary form
-  if (inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, SERVER_HERSCHEL_IP, &serverAddr.sin_addr) <= 0) {
     printf("\nInvalid address/ Address not supported \n");
     return -1;
   }
@@ -32,23 +34,66 @@ int main() {
     printf("\nConnection Failed \n");
   }
 
-  //receive the welcome and input user interface
-  recv(clientSocket, buffer, BUFFER_LEN, 0);
-  printf("%s\n",buffer);
-  memset(&buffer, '\0', sizeof(buffer));
-  recv(clientSocket, buffer, BUFFER_LEN, 0);
-  printf("%s\n",buffer);
-  memset(&buffer, '\0', sizeof(buffer));
-  char* userName;
-  //gets(userName);
+  //login into the server
+  int logInS = login(clientSocket,buffer);
 
-  while (1) {
+  while (logInS) {
     char *hello = "Hello from client";
     send(clientSocket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
     recv(clientSocket, buffer, BUFFER_LEN, 0);
     printf("The data rev : %s\n", buffer);
+    free(hello);
     break;
   }
+
   return 0;
+}
+
+
+int login(int socket, char* buffer) {
+  recv(socket, buffer, BUFFER_LEN, 0);
+  printf("%s\n",buffer);
+  memset(&buffer, '\0', sizeof(buffer));
+  recv(socket, buffer, BUFFER_LEN, 0);
+  printf("%s\n",buffer);
+  memset(&buffer, '\0', sizeof(buffer));
+
+  //get user name
+  char userName[USERNAME_LEN];
+  memset(&userName,'\0',sizeof(userName));
+  int count = 0;
+  fgets(userName,USERNAME_LEN,stdin);
+  send(socket,userName,strlen(userName),0);
+
+  int flag;
+  recv(socket,buffer,BUFFER_LEN,0);
+  flag = buffer[0] - '0';
+  memset(&buffer, '\0', sizeof(buffer));
+
+  
+  if (flag == 0) { //didn't find the user
+    recv(socket,buffer,BUFFER_LEN,0);
+    printf("%s\n",buffer);
+  } else { //find the user, next input the password
+    echo(false);
+    char password[PASSWORD_LEN];
+    fgets(password,PASSWORD_LEN,stdin);
+    echo(true);
+    send(socket,password,strlen(password),0);
+    recv(socket,buffer,BUFFER_LEN,0);
+    int pass = buffer[0] - '0';
+    
+  }
+  return 0;
+}
+  
+
+void echo(bool on) {
+  struct termios settings;
+  tcgetattr( STDIN_FILENO, &settings);
+  settings.c_lflag = on
+    ? (settings.c_lflag |   ECHO)
+    : (settings.c_lflag & ~(ECHO));
+  tcsetattr( STDIN_FILENO, TCSANOW, &settings);
 }
--- client/client.h
+++ client/client.h
@@ -1,14 +1,20 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <stdbool.h>
+
 #define BUFFER_LEN 1024
 #define PORT 3303
 //#define SERVER_IP "192.168.122.1"
 #define SERVER_HERSCHEL_IP "161.45.162.78"
 #define SERVER_RANGER_IP "161.XX.XX.XXX"
+#define USERNAME_LEN 20
+#define PASSWORD_LEN 50
 
+//log into the account
+int login(int socket, char* buffer);
 
-
+void echo(bool on);
 
 
 
