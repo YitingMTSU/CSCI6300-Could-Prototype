8f50d2dd4ea54e170817711c0e10aaa8a2729dc7
Yiting Wang <yw3c@herschel2.cs.mtsu.edu>
2020-03-14 19:51:25 -0500

    add selection of IP

--- client/client.c
+++ client/client.c
@@ -15,24 +15,42 @@ int main() {
   struct sockaddr_in serverAddr;
   char buffer[BUFFER_LEN];
   memset(buffer, 0, sizeof(buffer));
+
+  //choose the server you want to connect
+ SERVERCHOOSE:
+  printf("Which Server you want to connect? 1. HERSCHEL OR 2. RANGER\n");
+  printf("Type 1 OR 2: ");
+  char serverOption = getchar();
+  char* serverIP;
+  if (serverOption == '1') {
+    strcpy(serverIP,SERVER_HERSCHEL_IP);
+  } else if (serverOption == '2') {
+    strcpy(serverIP,SERVER_RANGER_IP);
+  } else {
+    printf("Cannot reconginze, choose again!\n");
+    goto SERVERCHOOSE;
+  }
   
   if ((clientSocket = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
     return -1;
   }
-
+  printf("[+]Create socket...\n");
+  
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(PORT);
 
   //Convert IPv4 and IPv6 address from text to binary form
-  if (inet_pton(AF_INET, SERVER_HERSCHEL_IP, &serverAddr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, serverIP, &serverAddr.sin_addr) <= 0) {
     printf("\nInvalid address/ Address not supported \n");
     return -1;
   }
+  printf("[+]Convert the IP...\n");
 
   if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
     printf("\nConnection Failed \n");
   }
+  printf("[+]Connect to the sever...\n");
 
   //login into the server
   int logInS = login(clientSocket,buffer);
@@ -50,16 +68,20 @@ int main() {
 int login(int socket, char* buffer) {
   //read the interface from server
   //int sig;
+  //int messageLen;
+  //recv(socket, &messageLen, sizeof(messageLen), 0);
+  //printf("the messagelen : %d\n",messageLen);
+  //sig = recv(socket, buffer, messageLen, 0);
   recv(socket, buffer, BUFFER_LEN, 0);
   printf("%s\n",buffer);
-  memset(buffer, 0, BUFFER_LEN);
-  //printf("\nrecv sig: %d\n",sig);
-
+  bzero(buffer, BUFFER_LEN);
+  //sleep(0.5);
+  
+  //recv(socket, &messageLen, sizeof(messageLen), 0);
   recv(socket, buffer, BUFFER_LEN, 0);
   printf("%s",buffer);
-  //printf("\nrecv sig: %d\n",sig);
   memset(buffer, 0, BUFFER_LEN);
-
+  
   //get user name
   char userName[USERNAME_LEN];
   memset(userName,0,USERNAME_LEN);
